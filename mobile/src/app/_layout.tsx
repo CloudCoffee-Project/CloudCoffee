4106d2e546e3112ab4de4ac335e995d4f7c4c039
Alvarez <vicente.alvarez2023@alu.uct.cl>
2026-09-06 20:16:09 -0300

    chore: limpiar variables de prueba de husky/eslint

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -8,8 +8,7 @@ export default function RootLayout() {
   const [sesion] = useState<SesionDecodificada | null>(null);
   const segments = useSegments();
   const router = useRouter();
-  const testVabUsada = 123;
-  const pru = 'hola';
+
   useEffect(() => {
     if (bootstrapping) return;
 
